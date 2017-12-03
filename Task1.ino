90e3aaabebc53a3cacc4a6ff5a3ae0e487388a99
namansharma14 <33182706+namansharma14@users.noreply.github.com>
2017-12-03 13:19:56 +0530

    Add files via upload

---
+++ Task1.ino
@@ -0,0 +1,50 @@
+#include<LiquidCystal.h>
+LiquidCrystal lcd(7,8,9,10,11,12)
+const int buttonPin=5;//up count
+const int buttonPin1=6;//down count
+
+int buttonPushCounter=0;//counts number of button pressed
+int buttonState=0;//current state of button
+int lastButtonState=0;//previus state of button
+
+void setup() {
+pinMode(buttonPin,INPUT);
+pinMode(buttonPin1,INPUT);  
+
+lcd.begin(16,2);
+lcd.setCursor(0,1)
+lcd.print("Counter: ");
+}
+
+void loop() {
+buttonState = digitalRead(buttonPin);
+if(buttonState!=lastButtonState) 
+ {
+  if(buttonState==HIGH)
+   {
+    buttonPushCounter++;
+    lcd.serCursor(7,1);
+    lcd.print(buttonPushCounter)
+     if(lastButtonState==9)
+     {
+       buttonState=0;
+     }
+   }
+ }
+lastButtonState=buttonState;
+
+if(buttonState!=lastButtonState) 
+ {
+  if(buttonState==HIGH)
+   {
+    buttonPushCounter--;
+    lcd.serCursor(7,1);
+    lcd.print(buttonPushCounter)
+     if(lastButtonState==3)
+     {
+       buttonState=6;
+     }
+   }
+ }
+lastButtonState=buttonState;
+}
